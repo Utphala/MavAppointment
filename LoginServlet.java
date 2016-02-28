c809587e82f70aa288c54dfb5a31f6f00d472e08
Utphala <utphala.p@gmail.com>
2016-02-27 23:29:37 -0600

    Added files via upload

--- LoginServlet.java
+++ LoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,6 +26,12 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		session = request.getSession();
+		// Create cookies for first and last names.      
+	     Cookie UserName = new Cookie("user_name",
+	                      request.getParameter("emailAddress"));
+	     UserName.setMaxAge(60*60*24);
+	     
+	     response.addCookie(UserName);
 		request.getRequestDispatcher("/login.jsp").forward(request,response);
 	}
 	
